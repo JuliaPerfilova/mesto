b82185730de18297ca4489d98774aac309f85239
Julia Perfilova <perfilovajulia@yandex.ru>
2022-04-01 22:54:45 +0300

    fix: fixed after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,35 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const editWindowClose = popup.querySelector('.popup__close-button');
-
-function togglePopup(event) {
-  if (event.target === event.currentTarget) {
-    popup.classList.toggle('popup_opened');
-  }
-}
-editButton.addEventListener('click', togglePopup);
-editWindowClose.addEventListener('click', togglePopup);
-popup.addEventListener('click', togglePopup);
-
-
-const likeButton = document.querySelector('.element__like-button');
-
-function toggleLikeButton() {
-  likeButton.classList.toggle('element__like-button_active');
-}
-likeButton.addEventListener('click', toggleLikeButton);
-
-const submitButton = popup.querySelector('.popup__save-button');
+const closeButton = popup.querySelector('.popup__close-button');
 const inputName = popup.querySelector('#name');
 const inputAbout = popup.querySelector('#about-person');
+const popupForm = popup.querySelector('.popup__form');
 
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileAbout = profile.querySelector('.profile__about');
 
+// Функция открытия окна Popup
+function openPopup() {
+  popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+}
+
+// Функция закрытия окна Popup
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+// Функция сохранения данных
 function saveProfile(event) {
   event.preventDefault();
   
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
-submitButton.addEventListener('click', saveProfile);
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', saveProfile);
